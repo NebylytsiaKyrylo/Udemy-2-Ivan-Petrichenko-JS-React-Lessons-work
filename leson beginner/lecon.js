a7ef524578b2da3091e54c8c4cf931b0d2315682
Kyrylo Nebylytsia <knebylytsia.it@gmail.com>
2022-07-26 20:03:42 +0200

    update js function

--- leson beginner/lecon.js
+++ leson beginner/lecon.js
@@ -1,22 +1,3 @@
-/* Assignment to the lesson:
-1) Create the numberOfFilms variable and place the user answer to the question:
-'How many movies have you seen? '
-2) Create a personalMovieDB object and place the following properties in it:
-    - count - here is the answer to the first question
-    - movies - place an empty object in this property
-    - actors - also place an empty object
-    - genres - place an empty array here
-    - privat - place boolean(logical) value false in this property
-3) Ask the user twice:
-    - 'One of the last movies seen? '
-    - 'How much would you rate him? '
-The answers should be placed in separate variables
-Write your answers to a movie object in the following format:
-    movies: {
-        'logan': '8.1'
-    }
-*/
-
 'use strict';
 
 const numberOfFilms = +prompt('How many movies have you seen?', '');
@@ -29,12 +10,27 @@ const personalMovieDB = {
   privat: false,
 };
 
-const a = prompt('One of the last movies seen?', ''),
-  b = prompt('How much would you rate him?', ''),
-  c = prompt('One of the last movies seen?', ''),
-  d = prompt('How much would you rate him?', '');
+for (let i = 0; i < 2; i++) {
+  const a = prompt('One of the last movies seen?', ''),
+    b = prompt('How much would you rate him?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+  if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+    console.log('done');
+  } else {
+    console.log('error');
+    i--;
+  }
+}
+
+if (personalMovieDB.count < 10) {
+  console.log('Not much films');
+} else if (personalMovieDB.count >= 10 || personalMovieDB <= 30) {
+  console.log('Normal count films');
+} else if (personalMovieDB.count > 30) {
+  console.log('Kinoman');
+} else {
+  console.log('Error');
+}
 
 console.log(personalMovieDB);
