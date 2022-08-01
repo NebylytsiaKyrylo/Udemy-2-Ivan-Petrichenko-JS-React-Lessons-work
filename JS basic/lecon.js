0686085cc4b284847c2fd0905aa9d28d649b0a93
Kyrylo Nebylytsia <knebylytsia.it@gmail.com>
2022-08-01 21:59:31 +0200

    Updated function in lecon.js

--- JS basic/lecon.js
+++ JS basic/lecon.js
@@ -1,72 +1,77 @@
 'use strict';
 
-let numberOfFilms;
-
-function start() {
-  numberOfFilms = +prompt('How many movies have you seen?', '').trim();
-
-  while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-    numberOfFilms = +prompt('How many movies have you seen?', '');
-  }
-}
-
-// start();
-
 const personalMovieDB = {
-  count: numberOfFilms,
+  count: 0,
   movies: {},
   actors: {},
   genres: [],
   privat: false,
-};
+  start: function () {
+    personalMovieDB.count = +prompt(
+      'How many movies have you seen?',
+      '',
+    ).trim();
 
-function questionsFn() {
-  for (let i = 0; i < 2; i++) {
-    const a = prompt('One of the last movies seen?', ''),
-      b = prompt('How much would you rate him?', '');
-
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-      personalMovieDB.movies[a] = b;
-      console.log('done');
-    } else {
-      console.log('error');
-      i--;
+    while (
+      personalMovieDB.count == '' ||
+      personalMovieDB.count == null ||
+      isNaN(personalMovieDB.count)
+    ) {
+      personalMovieDB.count = +prompt('How many movies have you seen?', '');
     }
-  }
-}
+  },
+  questionsFn: function () {
+    for (let i = 0; i < 2; i++) {
+      const a = prompt('One of the last movies seen?', ''),
+        b = prompt('How much would you rate him?', '');
 
-// questionsFn();
+      if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+      } else {
+        console.log('error');
+        i--;
+      }
+    }
+  },
+  yourLevelOfKinoman: function () {
+    if (personalMovieDB.count < 10) {
+      console.log('Not much films');
+    } else if (personalMovieDB.count >= 10 || personalMovieDB <= 30) {
+      console.log('Normal count films');
+    } else if (personalMovieDB.count > 30) {
+      console.log('Kinoman');
+    } else {
+      console.log('Error');
+    }
+  },
+  showMyDB: function (hidden) {
+    if (!hidden) {
+      console.log(personalMovieDB);
+    }
+  },
+  toggleVisibleMyDB: function () {
+    if (personalMovieDB.privat) {
+      personalMovieDB.privat = false;
+    } else {
+      personalMovieDB.privat = true;
+    }
+  },
+  writeYourGenres: function () {
+    for (let i = 1; i <= 3; i++) {
+      let genre = prompt(`Your favorite movie genre number ${i}`, '');
 
-function yourLevelOfKinoman() {
-  if (personalMovieDB.count < 10) {
-    console.log('Not much films');
-  } else if (personalMovieDB.count >= 10 || personalMovieDB <= 30) {
-    console.log('Normal count films');
-  } else if (personalMovieDB.count > 30) {
-    console.log('Kinoman');
-  } else {
-    console.log('Error');
-  }
-}
-
-// yourLevelOfKinoman();
-
-function showMyDB(hidden) {
-  if (!hidden) {
-    console.log(personalMovieDB);
-  }
-}
-
-// showMyDB(personalMovieDB.privat);
-
-function writeYourGenres() {
-  for (let i = 1; i <= 3; i++) {
-    personalMovieDB.genres.push(
-      prompt(`Your favorite movie genre number ${i}`, ''),
-    );
-  }
-}
-
-writeYourGenres();
+      if (genre === '' || genre == null) {
+        console.log('You entered incorrect data, try again, please');
+        i--;
+      } else {
+        personalMovieDB.genres[i - 1] = genre;
+      }
+    }
+    personalMovieDB.genres.forEach((genre, i) =>
+        console.log(`Your favorite movie genre ${i + 1} - ${genre}`),
+      );
+  },
+};
 
 console.log(personalMovieDB);
