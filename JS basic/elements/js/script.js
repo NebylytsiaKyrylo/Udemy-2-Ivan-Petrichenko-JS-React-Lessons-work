e12a8ed318f6703bc07eaec0bf461c360144d3dc
Kyrylo Nebylytsia <knebylytsia.it@gmail.com>
2022-08-05 19:55:29 +0200

    lesson document.querySelector

---
+++ JS basic/elements/js/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+let btns = document.querySelectorAll('button')[1];
+
+console.log(btns);
+
+let but = document.querySelector('button');
+
+console.log(but);
+
+let kvadr = document.querySelector('#box');
+console.log(kvadr);
