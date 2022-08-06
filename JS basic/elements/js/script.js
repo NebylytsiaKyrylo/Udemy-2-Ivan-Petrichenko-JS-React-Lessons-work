6875753274beb97a47c98126320afb39a27d23fa
Kyrylo Nebylytsia <knebylytsia.it@gmail.com>
2022-08-06 20:51:01 +0200

    work with elements on page

--- JS basic/elements/js/script.js
+++ JS basic/elements/js/script.js
@@ -3,9 +3,9 @@
 const box = document.getElementById('box'),
   btns = document.getElementsByTagName('button'),
   circles = document.getElementsByClassName('circle'),
-  hearts = document.querySelectorAll('.heart'),
-  oneHeart = document.querySelector('.heart'),
-  wrapper = document.querySelector('.wrapper');
+  wrapper = document.querySelector('.wrapper'),
+  hearts = wrapper.querySelectorAll('.heart'),
+  oneHeart = wrapper.querySelector('.heart');
 
 box.style.backgroundColor = 'blue';
 box.style.width = '500px';
@@ -20,8 +20,19 @@ const div = document.createElement('div');
 
 div.classList.add('black');
 
-document.body.append(div); //to end file
+// document.body.append(div); //to end body
 
-// wrapper.append(div); //to end class
-wrapper.prepend(div); //to start class
+wrapper.append(div); //to end class
+// wrapper.prepend(div); //to start class
 
+// circles[0].before(div); //before class circles
+// circles[1].after(div); //after heart i=1;
+
+// hearts[0].remove(); //delete element
+
+// btns[2].replaceWith(hearts[1]); //replace elements, btns[2] deleted from page and hearts[1] removed
+
+div.innerHTML = '<h1>Hello world</h1>'; // add html structure to div
+// div.textContent = 'Hello world'; //add text to div
+
+div.insertAdjacentElement("afterend", '<h2>Hello</h2>'); //insert element
