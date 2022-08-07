41f051334619319ee29da2f4b3762e9cf3829801
Kyrylo Nebylytsia <knebylytsia.it@gmail.com>
2022-08-07 21:07:35 +0200

    Nav DOM

---
+++ JS basic/navigation DOM/script.js
@@ -0,0 +1,14 @@
+// console.log(document.body);
+// console.log(document.head);
+// console.log(document.documentElement);
+// console.log(document.body.childNodes);
+// console.log(document.body.firstElementChild);
+// console.log(document.body.lastElementChild);
+
+// console.log(document.querySelector('#current').parentNode);
+
+// console.log(document.querySelector('#current').parentNode.parentNode);
+
+// console.log(document.querySelector('[data-current="3"]'));
+// console.log(document.querySelector('[data-current="3"]').nextElementSibling);
+// console.log(document.querySelector('[data-current="3"]').previousElementSibling);
