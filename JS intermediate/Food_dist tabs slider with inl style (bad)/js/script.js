f22ebff5288789abbaef497082c72ba10a0f4555
Kyrylo Nebylytsia <knebylytsia.it@gmail.com>
2022-08-25 13:14:45 +0200

    Timer V JS

--- JS intermediate/Food_dist tabs slider with inl style (bad)/js/script.js
+++ JS intermediate/Food_dist tabs slider with inl style (bad)/js/script.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
+  //tabs
   const tabs = document.querySelectorAll('.tabheader__item'),
     tabsContent = document.querySelectorAll('.tabcontent'),
     tabsParent = document.querySelector('.tabheader__items');
@@ -29,4 +30,52 @@ document.addEventListener('DOMContentLoaded', () => {
       });
     }
   });
+
+  //timer
+
+  const deadLine = '2022-08-26';
+
+  function getTimeRemaining(endtime) {
+    const total = Date.parse(endtime) - new Date(),
+      days = Math.floor(total / (1000 * 60 * 60 * 24)),
+      hours = Math.floor((total / (1000 * 60 * 60)) % 24),
+      minutes = Math.floor((total / 1000 / 60) % 60),
+      seconds = Math.floor((total / 1000) % 60);
+
+    return {
+      total,
+      days,
+      hours,
+      minutes,
+      seconds,
+    };
+  }
+
+  const getZero = timeTimer => (timeTimer > 9 ? timeTimer : '0' + timeTimer);
+
+  function setTimer(selector, endtime) {
+    const timer = document.querySelector('.timer'),
+      days = document.querySelector('#days'),
+      hours = document.querySelector('#hours'),
+      minutes = document.querySelector('#minutes'),
+      seconds = document.querySelector('#seconds'),
+      timeInterval = setInterval(updateTime, 1000);
+
+    updateTime();
+
+    function updateTime() {
+      const t = getTimeRemaining(endtime);
+
+      days.innerHTML = getZero(t.days);
+      hours.innerHTML = getZero(t.hours);
+      minutes.innerHTML = getZero(t.minutes);
+      seconds.innerHTML = getZero(t.seconds);
+
+      if (t.total <= 0) {
+        clearInterval(timeInterval);
+      }
+    }
+  }
+
+  setTimer('.timer', deadLine);
 });
